2d1af281c49000f3c29c9c53dd933dd662a7523c
KholodKhadeja <kholod.khadeja@hotmail.com>
2023-01-21 14:29:20 +0200

    devs

--- src/Components/ImageSection/ImageSection.jsx
+++ src/Components/ImageSection/ImageSection.jsx
@@ -6,9 +6,8 @@ const ImageSection = () => {
             <img src="https://github.com/KholodKhadeja/Masjed-altaqwa-learning/blob/main/client-side/src/images/header.png?raw=true"
              className="img-fluid" alt="صورة مسجد التقوى - الطيرة" />
 
-            <p className="centered fs-1 fw-semibold text-nowrap">البرنامج العلمي - مسجد التقوى الطيرة 
-            <br />
-            مساق في العقيدة والفقه
+            <p className="centered fs-2 fw-semibold text-nowrap">
+البرنامج العلمي الإيماني التربوي 
             </p>
         </div>
     );
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -28,13 +28,10 @@ const HomePage = () => {
 <button className="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button"
  role="tab" aria-controls="home-tab-pane" aria-selected="true">الدروس</button>
 </li>
-<li className="nav-item" role="presentation">
-<button className="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" 
-role="tab" aria-controls="profile-tab-pane" aria-selected="false">الدورات</button>
-</li>
+
 <li className="nav-item" role="presentation">
 <button className="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact-tab-pane" 
-type="button" role="tab" aria-controls="contact-tab-pane" aria-selected="false"> كتب للإثراء</button>
+type="button" role="tab" aria-controls="contact-tab-pane" aria-selected="false"> كتب وروابط</button>
 </li>
 <li className="nav-item" role="presentation">
 <button className="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact-tab-pane" 
@@ -45,7 +42,7 @@ type="button" role="tab" aria-controls="contact-tab-pane" aria-selected="false">
 <div className="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" 
 tabIndex={0}>
 {/* **********************ACCORDION************************* */}
-<div className="accordion" id="accordionExample">
+<div className="accordion mb-2" id="accordionExample">
     {currentLessons.map((item)=>(
         <AccordionItemComponent key={"less"+item._id}
         lessonName={item.lessonName}
@@ -54,21 +51,17 @@ tabIndex={0}>
         pdfDesc={item.pdfDesc}
          vocalLink={item.vocalLink}
           vocalDesc={item.vocalDesc}
+          num={item.num}
         />
     ))}
       
 {/* *******************************the end of the accordion ******************************** */}
 </div></div>
 
-<div className="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab"
-tabIndex={1}>
-    {/* courses tab */}
-   <img src="https://github.com/KholodKhadeja/altaqwaLearning-client/blob/main/src/images/building%20page.png?raw=true" 
-   className="img-fluid" alt="صفحة قيد التطوير"/>
-</div>
+
 
 <div className="tab-pane fade" id="contact-tab-pane" role="tabpanel" aria-labelledby="contact-tab"
-tabIndex={2}>
+tabIndex={0}>
     {/* book tab */}
     <ul class="list-group">
   <li class="list-group-item"><a href="https://archive.org/details/WAQmkaim">
@@ -77,9 +70,8 @@ tabIndex={2}>
 </div>
 
 <div className="tab-pane fade" id="disabled-tab-pane" role="tabpanel" aria-labelledby="disabled-tab"
-tabIndex={3}>
-   <img src="https://github.com/KholodKhadeja/altaqwaLearning-client/blob/main/src/images/building%20page.png?raw=true" 
-   className="img-fluid" alt="صفحة قيد التطوير"/>
+tabIndex={0}>
+   <p>محتوى هذا الصفحة متوفر للأعضاء فقط</p>
 </div>
 </div>
 </div>
